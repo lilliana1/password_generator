39047fe3f7e834264fc128b74e801af699ab27ae
Lilliana Ramos <lilliana.cramos@gmail.com>
2019-12-02 20:22:32 -0500

    copy to clipboard button enable

--- script.js
+++ script.js
@@ -83,8 +83,12 @@ function writePassword() {
   copyBtn.focus();
 }
 
+
 function copyToClipboard() {
-  // BONUS 
+  var copyText = document.getElementById("password");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999)
+  document.execCommand("copy");
 }
 
 // Add event listener to generate button
